81c74b825de5dce14e290ff3e0af5cd31bf2419f
Johnny <evgkisevgkis@gmail.com>
2023-12-15 11:33:22 +0500

    написание декоратора для требования авторизации

---
+++ decorators.py
@@ -0,0 +1,20 @@
+import jwt
+from flask import request, abort
+from constants import SECRET, ALGO
+
+
+def auth_required(func):
+    def wrapper(*args, **kwargs):
+        if 'Authorization' not in request.headers:
+            abort(401)
+
+        data = request.headers['Authorization']
+        token = data.split("Bearer ")[-1]
+        try:
+            jwt.decode(token, SECRET, algorithms=[ALGO])
+        except Exception as e:
+            print("JWT Decode Exception", e)
+            abort(401)
+        return func(*args, **kwargs)
+
+    return wrapper
