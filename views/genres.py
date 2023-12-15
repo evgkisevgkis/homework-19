584ea96f9e60176490c1495edee2eabb78a9fb2c
Johnny <evgkisevgkis@gmail.com>
2023-12-15 16:07:20 +0500

    добавление методов PUT, POST и DELETE для жанров

--- views/genres.py
+++ views/genres.py
@@ -1,7 +1,8 @@
 from flask_restx import Resource, Namespace
-from decorators import auth_required
-from dao.model.genre import GenreSchema
+from decorators import auth_required, admin_required
+from dao.model.genre import GenreSchema, Genre
 from implemented import genre_service
+from flask import request
 
 genre_ns = Namespace('genres')
 
@@ -14,11 +15,29 @@ class GenresView(Resource):
         res = GenreSchema(many=True).dump(rs)
         return res, 200
 
+    @admin_required
+    def post(self):
+        data = request.json
+        new_genre = genre_service.create(data)
+        return 'Created', 201
 
-@genre_ns.route('/<int:rid>')
+@genre_ns.route('/<int:gid>')
 class GenreView(Resource):
     @auth_required
-    def get(self, rid):
-        r = genre_service.get_one(rid)
+    def get(self, gid):
+        r = genre_service.get_one(gid)
         sm_d = GenreSchema().dump(r)
         return sm_d, 200
+
+    @admin_required
+    def put(self, gid):
+        data = request.json
+        if "id" not in data:
+            data["id"] = gid
+        genre_service.update(data)
+        return 'Updated', 204
+
+    @admin_required
+    def delete(self, gid):
+        genre_service.delete(gid)
+        return 'Deleted', 204
