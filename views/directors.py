b62a7f31a88dfa12f5232de8d1dc12247d91ef62
Johnny <evgkisevgkis@gmail.com>
2023-12-15 11:42:22 +0500

    добавление требования об аутентификации в представления режиссеров

--- views/directors.py
+++ views/directors.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource, Namespace
-
+from decorators import auth_required
 from dao.model.director import DirectorSchema
 from implemented import director_service
 
@@ -8,6 +8,7 @@ director_ns = Namespace('directors')
 
 @director_ns.route('/')
 class DirectorsView(Resource):
+    @auth_required
     def get(self):
         rs = director_service.get_all()
         res = DirectorSchema(many=True).dump(rs)
@@ -16,6 +17,7 @@ class DirectorsView(Resource):
 
 @director_ns.route('/<int:rid>')
 class DirectorView(Resource):
+    @auth_required
     def get(self, rid):
         r = director_service.get_one(rid)
         sm_d = DirectorSchema().dump(r)
